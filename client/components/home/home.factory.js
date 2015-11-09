096062b91bd3b0092307b3c3940c100c4d1a1988
Andrew Kishino <AndrewKishino@gmail.com>
2015-11-09 14:51:39 -0800

    Initial commit

---
+++ client/components/home/home.factory.js
@@ -0,0 +1,28 @@
+/* home.factory.js */
+(function() {
+
+  angular
+    .module('app')
+    .factory('homeFactory', homeFactory);
+
+    /**
+     * ferdFactory
+     * 
+     * @description Factory for interfacing with FerdX server for all available
+     *   modules with MegaFerd server.
+     * @return {Object} The factory
+     */
+    function homeFactory($q, $http) {
+
+      var factory = {
+
+      };
+
+      return factory;
+
+      ////////////////////////////////////////////////////////////
+      ////////////////////////////////////////////////////////////
+
+    }
+
+})();
